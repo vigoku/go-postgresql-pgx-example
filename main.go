dd901d7c3a2d14bf956f27a6f9f9de5f1c3e32b1
Vinayak Gokuldas Kudva <vinayak.kudva@gmail.com>
2021-12-08 16:01:56 +0530

    Scan row by row

--- main.go
+++ main.go
@@ -27,12 +27,28 @@ func main() {
 		os.Exit(1)
 	}
 	defer db.Close(context.Background())
-	var users []*User
-	pgxscan.Select(ctx, db, &users, `SELECT id, name, email, age FROM users`)
-	// users variable now contains data from all rows.
+	//GET ALL ROWS
+	// var users []*User
+	// pgxscan.Select(ctx, db, &users, `SELECT id, name, email, age FROM users`)
+	// // users variable now contains data from all rows.
 
-	for i := 0; i < len(users); i++ {
-		log.Print(i)
-		log.Println(users[i].ID, users[i].Name, users[i].Email, users[i].Age)
+	// for i := 0; i < len(users); i++ {
+	// 	log.Print(i)
+	// 	log.Println(users[i].ID, users[i].Name, users[i].Email, users[i].Age)
+	rows, _ := db.Query(ctx, `SELECT id, name, email, age FROM users`)
+	defer rows.Close()
+	for rows.Next() {
+		var user User
+		if err := pgxscan.ScanRow(&user, rows); err != nil {
+			// Handle row scanning error.
+			fmt.Fprintf(os.Stderr, "End of rows: %v\n", err)
+			return
+		}
+		// user variable now contains data from the current row.
+		log.Println(user.ID, user.Name, user.Email, user.Age)
+	}
+	if err := rows.Err(); err != nil {
+		// Handle rows final error.
+		fmt.Fprintf(os.Stderr, "End of rows: %v\n", err)
 	}
 }
